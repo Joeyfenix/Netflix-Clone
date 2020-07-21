f09de24b3fb2d47f419371a113fa595df3901ed4
Joeyfenix <60266518+Joeyfenix@users.noreply.github.com>
2020-07-21 19:44:02 +0530

    First Committ

---
+++ src/requests.js
@@ -0,0 +1,13 @@
+const API_KEY = 'ee2d0a1ab05510c50e1a4d1017499813'
+const instance = {
+  fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+  fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+  fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+  fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+  fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+  fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=37`,
+  fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+  fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+}
+
+export default instance
